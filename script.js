917c336e994be06c035ac5f9135cbcda457dca95
Dalexa <dalexasanjuanc@outlook.com>
2025-06-20 12:31:12 -0500

    Comentando y corrigiendo idioma de pagina

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//Obteniedndo valores y declarando
+//Obteniendo valores del DOM
 const containerHero = document.querySelector('#container-hero');
 const containerForm = document.querySelector('#container-request');
 const containerAdult = document.querySelector('#container-end');
@@ -17,28 +17,31 @@ let contenidoH3 = document.getElementById('content-h3')
 let refresh = document.getElementById('button-refresh');
 
 
+
+// Muestra el formulario al hacer clic en el botón inicial
 document.getElementById('button-hero').addEventListener('click', function() {
    containerHero.style.display = 'none';
    containerForm.style.display = 'block';
    
 });
 
+// Manejo del envío del formulario
 
 form.addEventListener('submit', function(e) {
     e.preventDefault(); 
     getInputValues();
 }); 
 
-
+// Función principal que gestiona los datos del formulario
 function getInputValues() {
     const name = nameInput.value;
     const age = parseInt(ageInput.value);
 
     if(isNaN(age)){
-      alert('Debes ingresar solo números')
+      alert('You must enter only numbers')
 
     }else if (age==""|| name=="" ){
-      alert('Debes llenar todos los campos')
+      alert('You must fill in all fields')
 
     }else if (age > 18) {
       adult(name,age)
@@ -46,26 +49,26 @@ function getInputValues() {
     }else if (age < 18){
       adult(name,age)
        adult(name,age)
-      contenidoH3.innerText  = "Eres menor de edad"
-      contenidoP.innerText = `¡Qué bien que ya estás aprendiendo desde ahora! Tienes todo un camino por delante para crecer, experimentar y mejorar cada día.
-                                Sigue así, nunca es muy pronto para empezar a construir tu futuro. `;
+      contenidoH3.innerText  = "You are a minor"
+      contenidoP.innerText = `¡It's great that you're already learning! You have a long way to go to grow, experiment, and improve every day.
+                              Keep going; it's never too early to start building your future. `;
     }else{
-     alert('Ha ocurrido un error')
+     alert('An error has occurred')
 
     }
   
 }
 
+// Muestra el mensaje general y oculta las demás vistas
 function adult(name, age){
-    contenidoH2.innerText = `Hola ${name}. Tienes ${age} de edad`;
+    contenidoH2.innerText = `Helli ${name}. You are ${age} years old`;
     containerHero.style.display = 'none';
     containerForm.style.display = 'none';
     containerAdult.style.display = 'block';
 
 }
 
-
-
+// Recarga la página al hacer clic en el botón de reinicio
 document.getElementById('button-refresh').addEventListener('click', function() {
     location.reload();
 });
